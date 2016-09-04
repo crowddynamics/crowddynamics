0595becaf3b94160ad1a8e57ec947212f47923ac
Jaan <jaan.tollander@gmail.com>
2016-09-04 15:32:46 +0300

    -

--- src/examples.py
+++ src/examples.py
@@ -78,42 +78,6 @@ class Hallway(MultiAgentSimulation):
         self.set_obstacles_to_linear_walls()
 
 
-@numba.jit(nopython=True)
-def _direction_update(agent, target, mid, r_mid, c_rect, r_rect):
-    target_direction = np.zeros(agent.shape)
-    for i in range(agent.size):
-        x = agent.position[i]
-        cond1 = (x - c_rect) <= r_rect
-        cond2 = length(mid - x) > r_mid
-        if np.all(cond1) and cond2:
-            # if inside walls and not inside circle near exit
-            target_direction[i] = normalize(target - x)
-        else:
-            target_direction[i] = np.array((1.0, 0.0))
-    return target_direction
-
-
-class Navigation:
-    def __init__(self, agent, door, door_width, width, height):
-        self.agent = agent
-        # Navigation algorithm
-        door1 = np.array(door[1], dtype=np.float64)
-        door0 = np.array(door[0], dtype=np.float64)
-        unit = normalize(door1 - door0)
-        normal = rotate90(unit)
-        mid = (door0 + door1) / 2  # Mid point of the two doors
-        r_max = 0.27  # Max agent radius
-        target = mid + r_max * normal
-        c_rect = r_rect = np.array((width / 2, height / 2))
-        r_mid = door_width / 2
-
-        self.func = partial(_direction_update, target=target, mid=mid,
-                            r_mid=r_mid, c_rect=c_rect, r_rect=r_rect)
-
-    def update(self):
-        self.agent.target_direction = self.func(self.agent)
-
-
 class RoomEvacuation(MultiAgentSimulation):
     def __init__(self, queue, size, width, height, model, body, spawn_shape,
                  door_width, exit_hall_width):
@@ -151,14 +115,6 @@ class RoomEvacuation(MultiAgentSimulation):
         self.set_body(size, body)
         self.set_model(model)
         self.set(**kwargs)
-
-        # target = np.asarray(exit_door)
-        # navigation = Navigation(self.agent,
-        #                         target,
-        #                         door_width,
-        #                         width,
-        #                         height)
-        # self.set_navigation(navigation)
 
         self.set_navigation("static")
         self.set_orientation()
