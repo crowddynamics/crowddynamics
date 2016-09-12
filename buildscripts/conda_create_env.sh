7dbe412cf3c1e018f6ac97183e82eaec13eecbf7
Jaan <jaan.tollander@gmail.com>
2016-09-12 19:44:53 +0300

    docs

--- buildscripts/conda_create_env.sh
+++ buildscripts/conda_create_env.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Create conda environment for crowd dynamics
-conda create --name crowd34 python=3.4
-source activate crowd34
+conda create --name crowd35 python=3.5
+source activate crowd35
 
 conda install yaml numpy numba matplotlib pyqt pyqtgraph h5py pandas scipy scikit-image shapely
-pip install scikit-fmm
+
 pip install ruamel.yaml
+pip install scikit-fmm
