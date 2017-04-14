b395fc25b15fef04e844974e51b49e4702b7a534
jaan <jaan.tollander@gmail.com>
2017-04-14 18:12:05 +0300

    continuous integration

---
+++ continuous_integration/travis/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Environment variables:
+#   CONDA_ENV: Name of the conda environment
+#   PYTHON: Version of python to use
+
+# Install Miniconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH=$HOME/miniconda/bin:$PATH
+source activate $CONDA_ENV
+conda update --yes conda
+
+# The next couple lines fix a crash with multiprocessing on Travis and are not specific to using Miniconda
+sudo rm -rf /dev/shm
+sudo ln -s /run/shm /dev/shm
+
+# Install conda environment from file
+conda env create \
+      --quiet \
+      --yes \
+      -n $CONDA_ENV \
+      python=$PYTHON \
+      -f environment-dev.yml
+
+# Install crowddynamics
+python setup.py install
---
+++ continuous_integration/travis/run_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run tests
+py.test
